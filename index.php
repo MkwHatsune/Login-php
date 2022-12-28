2129bd8127df61876e2cd88d1c51a00088cc4068
MkwHatsune <120064807+MkwHatsune@users.noreply.github.com>
2022-12-28 17:18:44 -0600

    Initial commit

---
+++ index.php
@@ -0,0 +1,23 @@
+<div class="login-box">
+  <h2>Login</h2>
+  <div class="wrapper"></div>
+  <link rel="stylesheet" href="styles/styles.css">
+  //https://codepen.io/nohoid/pen/eYXpNL
+  <form>
+    <div class="user-box">
+      <input type="text" name="" required="">
+      <label>Username</label>
+    </div>
+    <div class="user-box">
+      <input type="password" name="" required="">
+      <label>Password</label>
+    </div>
+    <a href="#">
+      <span></span>
+      <span></span>
+      <span></span>
+      <span></span>
+      Submit
+    </a>
+  </form>
+</div>
